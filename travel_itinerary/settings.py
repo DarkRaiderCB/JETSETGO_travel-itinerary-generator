9b7946c5a860e34e60b3470a60d8658830ecfc88
Sanyog Mishra <142327605+DarkRaiderCB@users.noreply.github.com>
2024-09-19 12:29:11 +0530

    Update settings.py

--- travel_itinerary/settings.py
+++ travel_itinerary/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-g9z%y$$fpzn0(4joml-b_wg6=(g(qms^l7a92^o6voqauov372
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
